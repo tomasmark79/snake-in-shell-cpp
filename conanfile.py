81e0b009fc3fc6de831cd3fb592feb9f10c416d9
tomasmark79 <tomas@digitalspace.name>
2025-02-19 13:40:51 +0100

    removed redundancy

--- conanfile.py
+++ conanfile.py
@@ -9,10 +9,10 @@ class MarkWareVCMake(ConanFile):
         self.options["*"].shared = False # this replaced shared flag from SolutionController and works
 
 
-    def requirements(self):
-        self.requires("zlib/1.2.11") # used to compress and decompress data
-        self.requires("fmt/11.1.1") # required by cpm package
-        self.requires("nlohmann_json/3.11.2") # is modern to have json support
+    #def requirements(self):
+        #self.requires("zlib/1.2.11") # used to compress and decompress data
+        #self.requires("fmt/11.1.1") # required by cpm package
+        #self.requires("nlohmann_json/3.11.2") # is modern to have json support
     # it is on you to define rest of the dependencies
 
     # -------------------------------------- --
